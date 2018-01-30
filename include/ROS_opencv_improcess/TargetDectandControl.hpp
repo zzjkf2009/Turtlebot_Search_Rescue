d08b11142d5b0fa48cb0209bc407ed9cc549e318
zzjkf2009 <zzeng@terpmail.umd.edu>
2018-01-30 11:46:31 -0500

    Update the constructor for ObjectOrientatedControl class

--- include/ROS_opencv_improcess/TargetDectandControl.hpp
+++ include/ROS_opencv_improcess/TargetDectandControl.hpp
@@ -1,26 +1,26 @@
 /********************************************************************
- *   MIT License
- *
- *   Copyright (c) 2017 Zejiang Zeng
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- ********************************************************************/
+*   MIT License
+*
+*   Copyright (c) 2017 Zejiang Zeng
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+********************************************************************/
 
 /** @file TargetDectandControl.hpp
 
@@ -37,92 +37,94 @@
 #ifndef INCLUDE_ROS_OPENCV_IMPROCESS_TARGETDECTANDCONTROL_HPP_
 #define INCLUDE_ROS_OPENCV_IMPROCESS_TARGETDECTANDCONTROL_HPP_
 
-#include<ros/ros.h>
+#include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/LaserScan.h>
-#include"opencv2/highgui/highgui.hpp"
-#include"opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
 class ObjectOrientatedControl {
- private:
-  ros::NodeHandle nh;
-  image_transport::Subscriber sub;
-  ros::Publisher pub;
-  ros::Subscriber ScanSub;
-  /**
-  * The number defined below are the HSV
-  * range for RED color
-  */
-  int iLowH = 0;
-  int iHighH = 10;
-  int iLowS = 40;
-  int iHighS = 255;
-  int iLowV = 0;
-  int iHighV = 100;
-  int weight;
-  bool collision;
-  float smallest;
+private:
+ros::NodeHandle nh;
+image_transport::Subscriber sub;
+ros::Publisher pub;
+ros::Subscriber ScanSub;
+/**
+ * The number defined below are the HSV
+ * range for RED color
+ */
+int iLowH;
+int iHighH;
+int iLowS = 40;
+int iHighS = 255;
+int iLowV = 0;
+int iHighV = 100;
+int weight;
+bool collision;
+float smallest;
 
- public:
-  int PoseX;
-  int PoseY;
-  bool find;
-  /**
-   *   @brief  constructor of the class
-   *
-   *  Initiate the ObjectOrientatedControl calss with its
-   *  subscriber and publisher
-   *   @param  ros::NodeHandle
-   *   @return double
-   */
-  explicit ObjectOrientatedControl();
-  /**
-   *   @brief  start the image subscriber
-   *
-   *   @param  none
-   *   @return none
-   */
-  void imageSubscriber();
-  /**
-   *   @brief  start the velocity Publisher
-   *
-   *   @param  none
-   *   @return none
-   */
-  void velocityPublisher();
-  /**
-   *   @brief  The callback function for the image subscriber
-   *
-   * Subscirbe the image from the turtlebot camera and convert the
-   * RGB image to HSV image, threshold the image to see if it can find
-   * the desired color. When the target is find, the positio will be get
-   * based on the getPosition function
-   *   @param  snesor_msgs
-   *   @return none
-   */
-  void imageCallback(const sensor_msgs::ImageConstPtr& msg);
-  /**
-   *   @brief  threshold the iamge by HSV
-   *
-   *   @param  cv::Mat
-   *   @return cv::Mat
-   */
-  cv::Mat getThreholdImg(cv::Mat imgOriginal);
-  /**
-   *   @brief  calculate the position based on the threshold
-   * image
-   *
-   *   @param  cv::Mat
-   *   @return none
-   */
-  void getPosition(const cv::Mat& imgThresholded);
-  /**
-   *   @brief  draw the detected location of the target in original
-   *   image
-   *
-   *   @param  cv::Mat
-   *   @return cv::Mat
-   */
-  cv::Mat drawPosition(cv::Mat originalImg);
+public:
+int PoseX;
+int PoseY;
+bool find;
+/**
+ *   @brief  constructor of the class
+ *
+ *  Initiate the ObjectOrientatedControl calss with its
+ *  subscriber and publisher
+ *   @param  ros::NodeHandle
+ *   @return double
+ */
+ObjectOrientatedControl(int iLowH = 0, int iHighH = 10);
+
+
+/**
+ *   @brief  start the image subscriber
+ *
+ *   @param  none
+ *   @return none
+ */
+void imageSubscriber();
+/**
+ *   @brief  start the velocity Publisher
+ *
+ *   @param  none
+ *   @return none
+ */
+void velocityPublisher();
+/**
+ *   @brief  The callback function for the image subscriber
+ *
+ * Subscirbe the image from the turtlebot camera and convert the
+ * RGB image to HSV image, threshold the image to see if it can find
+ * the desired color. When the target is find, the positio will be get
+ * based on the getPosition function
+ *   @param  snesor_msgs
+ *   @return none
+ */
+void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+/**
+ *   @brief  threshold the iamge by HSV
+ *
+ *   @param  cv::Mat
+ *   @return cv::Mat
+ */
+cv::Mat getThreholdImg(cv::Mat imgOriginal);
+/**
+ *   @brief  calculate the position based on the threshold
+ * image
+ *
+ *   @param  cv::Mat
+ *   @return none
+ */
+void getPosition(const cv::Mat& imgThresholded);
+/**
+ *   @brief  draw the detected location of the target in original
+ *   image
+ *
+ *   @param  cv::Mat
+ *   @return cv::Mat
+ */
+cv::Mat drawPosition(cv::Mat originalImg);
 };
 #endif  // INCLUDE_ROS_OPENCV_IMPROCESS_TARGETDECTANDCONTROL_HPP_
--- src/TargetDectandControl.cpp
+++ src/TargetDectandControl.cpp
@@ -1,26 +1,26 @@
 /********************************************************************
- *   MIT License
- *
- *   Copyright (c) 2017 Zejiang Zeng
- *
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- ********************************************************************/
+*   MIT License
+*
+*   Copyright (c) 2017 Zejiang Zeng
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+********************************************************************/
 
 /** @file TargetDectandControl.cpp
 
@@ -32,127 +32,129 @@
  *  @author Zejiang Zeng
  *  @date   12/2017
  */
-#include<ros/ros.h>
+#include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/LaserScan.h>
-#include"opencv2/highgui/highgui.hpp"
-#include"opencv2/imgproc/imgproc.hpp"
-#include"TargetDectandControl.hpp"
-#include"PIDcontroller.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "TargetDectandControl.hpp"
+#include "PIDcontroller.hpp"
 
-ObjectOrientatedControl::ObjectOrientatedControl() {
-  imageSubscriber();
-  pub = nh.advertise < geometry_msgs::Twist
-      > ("/cmd_vel_mux/input/teleop", 1000);
+
+ObjectOrientatedControl::ObjectOrientatedControl(int iLowH, int iHighH) : iLowH (iLowH)
+        ,iHighH (iHighH) {
+        imageSubscriber();
+        pub = nh.advertise < geometry_msgs::Twist
+                             > ("/cmd_vel_mux/input/teleop", 1000);
 }
 
 void ObjectOrientatedControl::imageSubscriber() {
-  image_transport::ImageTransport it(nh);
-  sub = it.subscribe("camera/rgb/image_raw", 1,
-                     &ObjectOrientatedControl::imageCallback, this);
+        image_transport::ImageTransport it(nh);
+        sub = it.subscribe("camera/rgb/image_raw", 1,
+                           &ObjectOrientatedControl::imageCallback, this);
 }
 
 void ObjectOrientatedControl::imageCallback(
-    const sensor_msgs::ImageConstPtr& msg) {
-  try {
-    ROS_INFO_STREAM("get image");
-    cv::Mat imgThresholded = getThreholdImg(
-        cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::imshow("Threshold", imgThresholded);
-    cv::waitKey(1);
-    getPosition(imgThresholded);
-    cv::Mat DrawedImg = drawPosition(cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::imshow("view", DrawedImg);
-    cv::waitKey(1);
-  } catch (cv_bridge::Exception& e) {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-  }
+        const sensor_msgs::ImageConstPtr& msg) {
+        try {
+                ROS_INFO_STREAM("get image");
+                cv::Mat imgThresholded = getThreholdImg(
+                        cv_bridge::toCvShare(msg, "bgr8")->image);
+                cv::imshow("Threshold", imgThresholded);
+                cv::waitKey(1);
+                getPosition(imgThresholded);
+                cv::Mat DrawedImg = drawPosition(cv_bridge::toCvShare(msg, "bgr8")->image);
+                cv::imshow("view", DrawedImg);
+                cv::waitKey(1);
+        } catch (cv_bridge::Exception& e) {
+                ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+        }
 }
 
 cv::Mat ObjectOrientatedControl::getThreholdImg(cv::Mat imgOriginal) {
-  weight = imgOriginal.cols;
-  cv::Mat imgHSV;
-  cv::Mat imgThresholded;
-  cv::namedWindow("control");
-  cvCreateTrackbar("LowH", "control", &iLowH, 255);
-  cvCreateTrackbar("HighH", "control", &iHighH, 255);
-  cvCreateTrackbar("LowS", "control", &iLowS, 255);
-  cvCreateTrackbar("HighS", "control", &iHighS, 255);
-  cvCreateTrackbar("LowV", "control", &iLowV, 255);
-  cvCreateTrackbar("HighV", "control", &iHighV, 255);
-  // Convert the captured frame from BGR to HSV
-  cv::cvtColor(imgOriginal, imgHSV, cv::COLOR_BGR2HSV);
-  // Threshold the image
-  cv::inRange(imgHSV, cv::Scalar(iLowH, iLowS, iLowV),
-              cv::Scalar(iHighH, iHighS, iHighV), imgThresholded);
-  // morphological opening  ( remove small objects from the foreground)
-  cv::erode(imgThresholded, imgThresholded,
-            cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-  cv::dilate(imgThresholded, imgThresholded,
-             cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-  // morphological closing  ( remove small holes from the foreground)
-  cv::dilate(imgThresholded, imgThresholded,
-             cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-  cv::erode(imgThresholded, imgThresholded,
-            cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-  return imgThresholded;
+        weight = imgOriginal.cols;
+        cv::Mat imgHSV;
+        cv::Mat imgThresholded;
+        cv::namedWindow("control");
+        cvCreateTrackbar("LowH", "control", &iLowH, 255);
+        cvCreateTrackbar("HighH", "control", &iHighH, 255);
+        cvCreateTrackbar("LowS", "control", &iLowS, 255);
+        cvCreateTrackbar("HighS", "control", &iHighS, 255);
+        cvCreateTrackbar("LowV", "control", &iLowV, 255);
+        cvCreateTrackbar("HighV", "control", &iHighV, 255);
+        // Convert the captured frame from BGR to HSV
+        cv::cvtColor(imgOriginal, imgHSV, cv::COLOR_BGR2HSV);
+        // Threshold the image
+        cv::inRange(imgHSV, cv::Scalar(iLowH, iLowS, iLowV),
+                    cv::Scalar(iHighH, iHighS, iHighV), imgThresholded);
+        // morphological opening  ( remove small objects from the foreground)
+        cv::erode(imgThresholded, imgThresholded,
+                  cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+        cv::dilate(imgThresholded, imgThresholded,
+                   cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+        // morphological closing  ( remove small holes from the foreground)
+        cv::dilate(imgThresholded, imgThresholded,
+                   cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+        cv::erode(imgThresholded, imgThresholded,
+                  cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
+        return imgThresholded;
 }
 
 void ObjectOrientatedControl::getPosition(const cv::Mat& imgThresholded) {
-  cv::Moments oMoments = cv::moments(imgThresholded);
-  double dM01 = oMoments.m01;
-  double dM10 = oMoments.m10;
-  double dArea = oMoments.m00;
-  PoseX = -1;
-  PoseY = -1;
-  if (dArea > 10000) {
-    // calculate the position of the object
-    PoseX = dM10 / dArea;
-    PoseY = dM01 / dArea;
-  }
+        cv::Moments oMoments = cv::moments(imgThresholded);
+        double dM01 = oMoments.m01;
+        double dM10 = oMoments.m10;
+        double dArea = oMoments.m00;
+        PoseX = -1;
+        PoseY = -1;
+        if (dArea > 10000) {
+                // calculate the position of the object
+                PoseX = dM10 / dArea;
+                PoseY = dM01 / dArea;
+        }
 }
 
 cv::Mat ObjectOrientatedControl::drawPosition(cv::Mat originalImg) {
-  if (PoseX > 0 && PoseY > 0) {
-    find = true;
-    cv::circle(originalImg, cv::Point(PoseX, PoseY), 10, (255, 255, 255), 1, 8,
-               0);
-  } else {
-    find = false;
-  }
-  return originalImg;
+        if (PoseX > 0 && PoseY > 0) {
+                find = true;
+                cv::circle(originalImg, cv::Point(PoseX, PoseY), 10, (255, 255, 255), 1, 8,
+                           0);
+        } else {
+                find = false;
+        }
+        return originalImg;
 }
 
 void ObjectOrientatedControl::velocityPublisher() {
-  PIDcontroller pid;
-  geometry_msgs::Twist input;
-  if (find) {
-    double error = weight / 2 - PoseX;
-    double out = pid.calculatePID(error);
-    if (abs(error) > weight / 4) {
-      input.linear.x = 0.0;
-      input.linear.y = 0;
-      input.linear.z = 0;
-      input.angular.x = 0;
-      input.angular.y = 0;
-      input.angular.z = out;
-    } else {
-      input.linear.x = 1.0;
-      input.linear.y = 0;
-      input.linear.z = 0;
-      input.angular.x = 0;
-      input.angular.y = 0;
-      input.angular.z = out;
-    }
-  } else {
-    input.linear.x = 0;
-    input.linear.y = 0;
-    input.linear.z = 0;
-    input.angular.x = 0;
-    input.angular.y = 0;
-    input.angular.z = 0.8;
-  }
-  pub.publish(input);
+        PIDcontroller pid;
+        geometry_msgs::Twist input;
+        if (find) {
+                double error = weight / 2 - PoseX;
+                double out = pid.calculatePID(error);
+                if (abs(error) > weight / 4) {
+                        input.linear.x = 0.0;
+                        input.linear.y = 0;
+                        input.linear.z = 0;
+                        input.angular.x = 0;
+                        input.angular.y = 0;
+                        input.angular.z = out;
+                } else {
+                        input.linear.x = 1.0;
+                        input.linear.y = 0;
+                        input.linear.z = 0;
+                        input.angular.x = 0;
+                        input.angular.y = 0;
+                        input.angular.z = out;
+                }
+        } else {
+                input.linear.x = 0;
+                input.linear.y = 0;
+                input.linear.z = 0;
+                input.angular.x = 0;
+                input.angular.y = 0;
+                input.angular.z = 0.8;
+        }
+        pub.publish(input);
 }
